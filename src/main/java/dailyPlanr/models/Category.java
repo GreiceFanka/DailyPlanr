9ef06a3dea7b5c4612dabe4d76739b968ac50ee7
GreiceFanka <greicefcardoso@gmail.com>
2023-06-05 14:07:30 +0200

    category entity

---
+++ src/main/java/dailyPlanr/models/Category.java
@@ -0,0 +1,56 @@
+package dailyPlanr.models;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(unique = true)
+	private String name;
+	
+	@OneToMany(mappedBy = "categories")
+	private List<Task> tasks;
+	
+	public Category() {
+		
+	}
+
+	public Category(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
+}
