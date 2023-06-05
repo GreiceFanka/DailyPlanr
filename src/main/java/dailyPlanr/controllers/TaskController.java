a7a46c6bfb3e4fda821223329db2ac1eabd217fe
GreiceFanka <greicefcardoso@gmail.com>
2023-06-05 14:17:58 +0200

    Add a inject for user session verification

--- src/main/java/dailyPlanr/controllers/TaskController.java
+++ src/main/java/dailyPlanr/controllers/TaskController.java
@@ -1,31 +1,56 @@
 package dailyPlanr.controllers;
 
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import dailyPlanr.models.Category;
+import dailyPlanr.models.CategoryRepository;
 import dailyPlanr.models.Task;
 import dailyPlanr.models.TaskRepository;
 import dailyPlanr.models.User;
 import jakarta.validation.Valid;
 
 @Controller
-@RequestMapping("/admin")
 public class TaskController {
 	@Autowired
 	private TaskRepository taskRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Inject
+	private LoggedUser loggedUser;
+	
 	@GetMapping("/newtask")
-	public String tasks() {
-		return "/newtask";
+	public String tasks(RedirectAttributes redirAttrs) {
+		boolean session = loggedUser.isLogged();
+		Iterable<Category> category = categoryRepository.findAll();
+		if(session) {			
+			return "/newtask";
+		}
+		return "redirect:/login";
 	}
 	
-	@PostMapping("/newtask")
-	public String newTask(@Valid Task task,User user) {
-		task.addUser(user);
-		taskRepository.save(task);
-		return "/newtask";
+	@PostMapping("/newtask/create/")
+	public String newTask(@Valid Task task,@Valid User user, RedirectAttributes redirAttrs) {
+		if(user != null) {
+			task.addUser(user);
+			taskRepository.save(task);
+			return "/newtask";
+		}else {
+			redirAttrs.addFlashAttribute("error", "Something went wrong.");
+			return "/newtask";
+		}
+	}
+	
+	@GetMapping("/alltasks")
+	public @ResponseBody Iterable<Task> getAllTasks() {
+		return taskRepository.findAll();
 	}
 }
