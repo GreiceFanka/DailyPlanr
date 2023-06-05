e21f51efa34f8154c2b7b2b92a8b531e0f283d21
GreiceFanka <greicefcardoso@gmail.com>
2023-06-05 14:37:26 +0200

    erased

--- src/main/java/DailyPlanr/models/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package DailyPlanr.models;
-
-import java.util.Optional;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-	public Optional<User> findByLogin(String login);
-}
