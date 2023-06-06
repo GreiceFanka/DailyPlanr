881dad7df2f61d12f1d3baca1861fca336d282eb
GreiceFanka <greicefcardoso@gmail.com>
2023-06-06 18:28:35 +0200

    Method implementation

--- src/main/java/dailyPlanr/models/CategoryRepository.java
+++ src/main/java/dailyPlanr/models/CategoryRepository.java
@@ -1,7 +1,11 @@
 package dailyPlanr.models;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer>{
-
+	public List<Category> findAll();
 }
