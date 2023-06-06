c1f6f2e03ce3d7dbce0c86ed90c7883b0be75d2f
GreiceFanka <greicefcardoso@gmail.com>
2023-06-06 18:23:13 +0200

    add. query to find tasks by user

--- src/main/java/dailyPlanr/models/TaskRepository.java
+++ src/main/java/dailyPlanr/models/TaskRepository.java
@@ -1,7 +1,12 @@
 package dailyPlanr.models;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TaskRepository extends CrudRepository<Task, Integer>{
-
+	
+	@Query(value= "select * from task tk\n"
+			+ "inner join task_user tku ON tk.id=tku.task_id\n"
+			+ "where user_id = ? ", nativeQuery = true)
+	public Iterable<Task> findTaskByUser(int id); 
 }
