e70095c51e0e005530c979dededfd78d3515b425
GreiceFanka <greicefcardoso@gmail.com>
2023-06-07 18:06:39 +0200

    get mapping all categories - add user_id in create categories

--- src/main/java/dailyPlanr/controllers/CategoryController.java
+++ src/main/java/dailyPlanr/controllers/CategoryController.java
@@ -1,13 +1,18 @@
 package dailyPlanr.controllers;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dailyPlanr.models.Category;
 import dailyPlanr.models.CategoryRepository;
+import dailyPlanr.models.User;
 import jakarta.validation.Valid;
 @RestController
 public class CategoryController {
@@ -15,13 +20,26 @@ public class CategoryController {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Inject
+	private LoggedUser loggedUser;
+	
 	@GetMapping("/allcategories")
-	public @ResponseBody Iterable<Category> getAllCategories(){
-		return categoryRepository.findAll();
+	public String getAllCategories(ModelMap model){
+		boolean session = loggedUser.isLogged();
+		int id = loggedUser.getUserId();
+		List<Category> categories = categoryRepository.findCategoryByUser(id);
+		if(session) {
+			model.addAttribute("login", loggedUser.getLoginUser());
+			model.addAttribute("user", loggedUser.getUserId());
+			model.addAttribute("categories", categories);
+			return "/allcategories";
+		}
+		return "redirect:/login";
 	}
 	
 	@PostMapping("/create/categories")
-		public String createCategory(@Valid Category category) {
+		public String createCategory(@Valid Category category, @Valid User user) {
+			category.addUsersCategory(user);
 			categoryRepository.save(category);
 			return "salvo";
 	}
