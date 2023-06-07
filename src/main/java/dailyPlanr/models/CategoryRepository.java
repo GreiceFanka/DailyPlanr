5f5aba60027338aa32640d3c7ff1ac2c1af6149b
GreiceFanka <greicefcardoso@gmail.com>
2023-06-07 18:04:09 +0200

    add abstract method to find categories by user

--- src/main/java/dailyPlanr/models/CategoryRepository.java
+++ src/main/java/dailyPlanr/models/CategoryRepository.java
@@ -2,10 +2,14 @@ package dailyPlanr.models;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer>{
-	public List<Category> findAll();
+	@Query(value= "select * from category ct\n"
+			+ "inner join categories_user ctu ON ct.id=ctu.category_id\n"
+			+ "where user_id = ? ", nativeQuery = true)
+	public List<Category> findCategoryByUser(int id);
 }
