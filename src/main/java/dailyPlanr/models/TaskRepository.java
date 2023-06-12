4574f893f242ebf3cd1f63338049afbad546cb25
GreiceFanka <greicefcardoso@gmail.com>
2023-06-12 18:06:51 +0200

    abstract method to edit a task

--- src/main/java/dailyPlanr/models/TaskRepository.java
+++ src/main/java/dailyPlanr/models/TaskRepository.java
@@ -1,7 +1,11 @@
 package dailyPlanr.models;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TaskRepository extends CrudRepository<Task, Integer>{
 	
@@ -9,4 +13,12 @@ public interface TaskRepository extends CrudRepository<Task, Integer>{
 			+ "inner join task_user tku ON tk.id=tku.task_id\n"
 			+ "where user_id = ? ", nativeQuery = true)
 	public Iterable<Task> findTaskByUser(int id); 
+	
+	@Transactional
+	@Modifying
+	@Query(value="UPDATE task SET data = ? , title = ? , description = ? ,task_status = ? , category_id = ? "
+			+ "WHERE id = ? ", nativeQuery = true)
+	public void updateTask(String data, String title, String description, String taskStatus, int cat_id, int task_id);
+	
+	public List<Task> findTaskById(int id);
 }
