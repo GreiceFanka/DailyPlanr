4b37e32199e182135b57b4e63fc67c07a73a8de0
GreiceFanka <greicefcardoso@gmail.com>
2023-06-21 17:42:04 +0200

    add route to get late tasks - modified allTasks route to generated alert

--- src/main/java/dailyPlanr/controllers/TaskController.java
+++ src/main/java/dailyPlanr/controllers/TaskController.java
@@ -28,24 +28,23 @@ import jakarta.validation.Valid;
 public class TaskController {
 	@Autowired
 	private TaskRepository taskRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Inject
 	private LoggedUser loggedUser;
-	
+
 	@Inject
 	private Mail mail;
-	
-	
+
 	@GetMapping("/newtask")
 	public String tasks(ModelMap model) {
 		boolean session = loggedUser.isLogged();
 		int id = loggedUser.getUserId();
-		if(session) {	
+		if (session) {
 			List<Category> listCategories = categoryRepository.findCategoryByUser(id);
-			
+
 			model.addAttribute("login", loggedUser.getLoginUser());
 			model.addAttribute("user", loggedUser.getUserId());
 			model.addAttribute("categories", listCategories);
@@ -53,85 +52,115 @@ public class TaskController {
 		}
 		return "redirect:/login";
 	}
-	
+
 	@PostMapping("/newtask/create/")
-	public String newTask(@Valid Task task,@Valid User user, RedirectAttributes redirAttrs) {
-		if(user != null) {
+	public String newTask(@Valid Task task, @Valid User user, RedirectAttributes redirAttrs) {
+		if (user != null) {
 			task.addUser(user);
 			taskRepository.save(task);
-			redirAttrs.addFlashAttribute("success","Everything went just fine.");
+			redirAttrs.addFlashAttribute("success", "Everything went just fine.");
 			return "redirect:/newtask";
-		}else {
+		} else {
 			redirAttrs.addFlashAttribute("error", "Something went wrong, try again.");
 			return "/newtask";
 		}
 	}
-	
+
 	@GetMapping("/alltasks")
 	public String getAllTasks(ModelMap model) {
+		String alert;
 		boolean session = loggedUser.isLogged();
 		int id = loggedUser.getUserId();
 		Iterable<Task> allTasks = taskRepository.findTaskByUser(id);
-		
-		if(session) {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		now.format(dateTimeFormatter);
+
+		for (Task task : allTasks) {
+			int latedTasks = task.getData().compareTo(now);
+			boolean toDoStatus = task.getTaskStatus().equalsIgnoreCase("To do");
+			boolean inProgressStatus = task.getTaskStatus().equalsIgnoreCase("In progress");
+
+			if (latedTasks <= -1 && toDoStatus || inProgressStatus) {
+				alert = "You have late tasks!";
+				model.addAttribute("alert", alert);
+			} else {
+				alert = "null";
+				model.addAttribute("alert", alert);
+			}
+		}
+
+		if (session) {
 			model.addAttribute("login", loggedUser.getLoginUser());
 			model.addAttribute("user", loggedUser.getUserId());
 			model.addAttribute("tasks", allTasks);
-			
 			return "/alltasks";
 		}
 		return "redirect:/login";
 	}
-	
-	
+
 	@GetMapping("/delete/task/{id}")
 	public String deleteTask(@PathVariable int id) {
 		taskRepository.deleteById(id);
 		return "redirect:/alltasks";
 	}
-	
+
 	@GetMapping("/edit/task/{id}")
 	public String editTask(@PathVariable int id, ModelMap model, Status status) {
 		int user_id = loggedUser.getUserId();
-		
+
 		List<Task> tasks = taskRepository.findTaskById(id);
 		List<Category> listCategories = categoryRepository.findCategoryByUser(user_id);
 		List<String> allStatus = Status.getAllStatus();
-		
+
 		model.addAttribute("login", loggedUser.getLoginUser());
 		model.addAttribute("user", loggedUser.getUserId());
 		model.addAttribute("tasks", tasks);
 		model.addAttribute("categories", listCategories);
-		model.addAttribute("status",allStatus);
+		model.addAttribute("status", allStatus);
 		return "/updatetask";
 	}
-	
+
 	@PostMapping("/update/task")
-	public String updateTask(@RequestParam String data,@RequestParam String title,@RequestParam String description,@RequestParam int task_id) {
+	public String updateTask(@RequestParam String data, @RequestParam String title, @RequestParam String description,
+			@RequestParam int task_id) {
 		taskRepository.updateTask(data, title, description, task_id);
 		return "redirect:/alltasks";
 	}
-	
+
 	@PostMapping("edit/status")
-	public String editTaskStatus(@RequestParam String taskStatus,@RequestParam int id) {
+	public String editTaskStatus(@RequestParam String taskStatus, @RequestParam int id) {
 		taskRepository.editStatus(taskStatus, id);
 		return "redirect:/alltasks";
 	}
-	
+
 	@PostMapping("edit/category")
-	public String editTaskCategory(@RequestParam int cat_id,@RequestParam int id){
+	public String editTaskCategory(@RequestParam int cat_id, @RequestParam int id) {
 		taskRepository.editTaskCategory(cat_id, id);
 		return "redirect:/alltasks";
 	}
-	
+
 	@GetMapping("/sendReminder")
 	public void sendReminder() throws EmailException {
 		LocalDateTime date = LocalDateTime.now().plusDays(1);
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
 		date.format(dateTimeFormatter);
-		
+
 		List<String> userList = taskRepository.findTaskByDate(date);
 		String passwordMail = mail.getPasswordMail();
 		Task.sendEmail(userList, passwordMail);
+	}
+	
+	@GetMapping("/late/tasks")
+	public String lateTasks(ModelMap model) {
+		int id = loggedUser.getUserId();
+		LocalDateTime date = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		date.format(dateTimeFormatter);
+		
+		Iterable<Task> lateTasks = taskRepository.findLateTasks(id, date);
+		model.addAttribute("lateTasks", lateTasks);
+		model.addAttribute("login", loggedUser.getLoginUser());
+		return "/lateTask";
 	}
 }
