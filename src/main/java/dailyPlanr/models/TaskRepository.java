c72694eca919ca4e78f041f1b61c80a66262de9d
GreiceFanka <greicefcardoso@gmail.com>
2023-06-22 17:25:09 +0200

    add insert query

--- src/main/java/dailyPlanr/models/TaskRepository.java
+++ src/main/java/dailyPlanr/models/TaskRepository.java
@@ -10,37 +10,43 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface TaskRepository extends CrudRepository<Task, Integer>{
 	
-	@Query(value= "select * from task tk\n"
+	@Query(value= "select * from Task tk\n"
 			+ "inner join task_user tku ON tk.id=tku.task_id\n"
 			+ "where user_id = ? ", nativeQuery = true)
 	public Iterable<Task> findTaskByUser(int id); 
 	
-	@Query(value = "SELECT DISTINCT u.login FROM user u\n"
+	@Query(value = "SELECT DISTINCT u.login FROM User u\n"
 			+ "LEFT JOIN task_user tu ON u.id=tu.user_id\n"
-			+ "LEFT JOIN task t ON t.id=tu.task_id\n"
-			+ "WHERE t.data <= ? AND t.task_status IN ('In progress', 'To do')", nativeQuery = true)
+			+ "LEFT JOIN Task t ON t.id=tu.task_id\n"
+			+ "WHERE t.data <= ? AND t.taskStatus IN ('In progress', 'To do')", nativeQuery = true)
 	public List<String> findTaskByDate(LocalDateTime date);
 	
-	@Query(value="SELECT * FROM task tk "
+	@Query(value="SELECT * FROM Task tk "
 			+ "INNER JOIN task_user tku ON tk.id=tku.task_id "
-			+ "WHERE tku.user_id = ? AND tk.data < ?  AND tk.task_status IN ('In progress', 'To do')", nativeQuery = true)
+			+ "WHERE tku.user_id = ? AND tk.data < ?  AND tk.taskStatus IN ('In progress', 'To do')", nativeQuery = true)
 	public Iterable<Task> findLateTasks(int id, LocalDateTime date);
 	
 	@Transactional
 	@Modifying
-	@Query(value="UPDATE task SET data = ? , title = ? , description = ?  "
+	@Query(value="UPDATE Task SET data = ? , title = ? , description = ?  "
 			+ "WHERE id = ? ", nativeQuery = true)
 	public void updateTask(String data, String title, String description, int task_id);
 	
 	@Transactional
 	@Modifying
-	@Query(value="UPDATE task SET task_status = ? WHERE id = ? ", nativeQuery = true)
+	@Query(value="UPDATE Task SET taskStatus = ? WHERE id = ? ", nativeQuery = true)
 	public void editStatus(String taskStatus, int id);
 	
 	@Transactional
 	@Modifying
-	@Query(value="UPDATE task SET category_id = ? WHERE id = ? ", nativeQuery = true)
+	@Query(value="UPDATE Task SET category_id = ? WHERE id = ? ", nativeQuery = true)
 	public void editTaskCategory(int cat_id, int id);
+	
+	@Transactional
+	@Modifying
+	@Query(value="INSERT INTO task_user (task_id, user_id) "
+			+ "VALUES (?, ?) ", nativeQuery = true)
+	public void insertUserTask(int taskId, int userId);
 	
 	public List<Task> findTaskById(int id);
 }
