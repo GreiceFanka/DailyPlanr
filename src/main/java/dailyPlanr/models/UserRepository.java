bb1ec8a38be846a8e3e109fc32cd3440c3670222
GreiceFanka <greicefcardoso@gmail.com>
2023-06-22 17:27:14 +0200

    add select query

--- src/main/java/dailyPlanr/models/UserRepository.java
+++ src/main/java/dailyPlanr/models/UserRepository.java
@@ -2,8 +2,12 @@ package dailyPlanr.models;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
+	@Query(value="SELECT * FROM User WHERE company = ? ", nativeQuery = true)
+	public Iterable<User> findUserWithSameCompany(String company);
+	
 	public Optional<User> findByLogin(String login);
 }
