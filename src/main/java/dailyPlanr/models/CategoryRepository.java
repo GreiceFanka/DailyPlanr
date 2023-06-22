bed9a846859dd8102f347d329045f23f2e94c90c
GreiceFanka <greicefcardoso@gmail.com>
2023-06-22 17:19:01 +0200

    changed table name and parameter name

--- src/main/java/dailyPlanr/models/CategoryRepository.java
+++ src/main/java/dailyPlanr/models/CategoryRepository.java
@@ -10,16 +10,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer>{
-	@Query(value= "select * from category ct\n"
+	@Query(value= "select * from Category ct\n"
 			+ "inner join categories_user ctu ON ct.id=ctu.category_id\n"
 			+ "where user_id = ? ", nativeQuery = true)
 	public List<Category> findCategoryByUser(int id);
 	
 	@Transactional
 	@Modifying
-	@Query(value="UPDATE category SET name = ? "
+	@Query(value="UPDATE Category SET categoryName = ? "
 			+ "WHERE id = ? ", nativeQuery = true)
-	public void updateCategory(String name, int id);
+	public void updateCategory(String categoryName, int id);
 	
 	public List<Category> findCategoryById(int id);
 }
