b34f555853b57e058cc93879ceb2444eb9cd60b4
GreiceFanka <greicefcardoso@gmail.com>
2023-06-26 18:03:56 +0200

    script to vertical menu and modal alert

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,25 @@
+	$('.dropdown-toggle').dropdown();
+		if ($('#alert').text() != 'null') {
+			$('#alertModal').modal('show');
+		}
+
+
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+})(jQuery);
