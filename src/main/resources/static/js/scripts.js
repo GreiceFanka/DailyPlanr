da95fab09b9b6a7a49fdc524ba7c65ef7b4728b3
GreiceFanka <greicefcardoso@gmail.com>
2023-07-11 17:18:18 +0200

    add functions to move element event

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -38,3 +38,40 @@
 	$(".close").click(function() {
 		$("#modalStatus").modal('hide');
 	});
+
+	const cards = document.querySelectorAll('#tasks');
+	const lists = document.querySelectorAll('#kanban');
+	
+	cards.forEach(card => {
+  	card.addEventListener('dragstart', dragStart);
+  	card.addEventListener('dragend', dragEnd);
+	});
+	
+	
+	function dragStart() {
+		lists.forEach(list => list.classList.add('highlight'));
+  		this.classList.add('dragging');
+	}
+	function dragEnd() {
+		lists.forEach(list => list.classList.remove('highlight'));
+  		this.classList.remove('dragging');
+	}
+	
+	lists.forEach( list => {
+  	list.addEventListener('dragover', dragover);
+  	list.addEventListener('dragleave', dragleave);
+  	list.addEventListener('drop', drop);
+	});
+	
+	function dragover() {
+		this.classList.add('over');
+  		const cardBeingDragged = document.querySelector('.dragging');
+  		this.appendChild(cardBeingDragged);
+	}
+	function dragleave() { 
+		this.classList.remove('over');
+	}
+	function drop() {
+		this.classList.remove('over');
+	 }
+	
