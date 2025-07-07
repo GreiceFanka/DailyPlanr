2878b66773570486b6cdee84992a484ce02324ab
GreiceFanka <greicefcardoso@gmail.com>
2025-07-07 14:36:31 +0100

    deleted spaces

--- src/main/java/dailyplanr/DailyPlanrApplication.java
+++ src/main/java/dailyplanr/DailyPlanrApplication.java
@@ -10,8 +10,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class DailyPlanrApplication {
 	
+	
 	public static void main(String[] args) {
-		SpringApplication.run(DailyPlanrApplication.class, args);}
+		SpringApplication.run(DailyPlanrApplication.class, args);
+	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
